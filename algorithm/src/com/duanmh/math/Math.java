981114eed3b471fb2b92cf16e65524d319d94753
ronlygithub <duanmenghua@126.com>
2014-12-14 13:50:00 +0800

    牛顿迭代

--- algorithm/src/com/duanmh/math/Math.java
+++ algorithm/src/com/duanmh/math/Math.java
@@ -24,20 +24,28 @@
  ******************************************************************
  */
 
-package com.duanmh.power;
+package com.duanmh.math;
 
 /**
  * @author duanmh
  * 
  */
-public class Power {
+public class Math {
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
-		System.out.println(power(0, 0));
-		System.out.println(power(1, 0));
-		System.out.println(power(2.5, 100));
+//		System.out.println(power(0, 0));
+//		System.out.println(power(1, 0));
+//		System.out.println(power(2.5, 100));
+		int value = Integer.MAX_VALUE;
+		System.out.println("my: "+sqrt(value));
+		
 		long end = System.currentTimeMillis();
 		System.out.println("time spent: " + (end - start));
+		System.out.println("system: "+java.lang.Math.sqrt(value));
+		long current = System.currentTimeMillis();
+		System.out.println("time spent: " + (current - end));
+		System.out.println(sqrtNewTon(value));
+		System.out.println("trible: "+ value +"  " + tribleNewTon(125));
 	}
 
 	private static Double power(double base, int expent) {
@@ -83,6 +91,58 @@ public class Power {
 		}
 		return false;
 
+	}
+	
+	public static Double sqrt(double value){
+		double low = 0;
+		double high = value;
+		double result = 0;
+		double middle = 0;
+		int count = 0;
+		do {
+			middle = low + ((high-low)/2);
+			result = middle*middle;
+			if (result > value) {
+				high = middle;
+			}else if(result < value){
+				low = middle;
+			}
+			count++;
+		} while (java.lang.Math.abs(result - value) > 0.000000000001);
+		
+		System.out.println("count: " + count);
+		return middle;		
+	}
+	
+	public static Double sqrtNewTon(double value){
+		
+		double tmp = 1;		
+		double result = 10;
+		int count = 0;
+		do {
+			tmp = result;
+			result = (result + value/result)/2;
+			count++;
+			
+		} while (java.lang.Math.abs(result - tmp) > 0.000000000001);
+		System.out.println("count: " + count);
+		return result;
+		
+	}
+	public static Double tribleNewTon(double value){
+		
+		double tmp = 1;		
+		double result = 10;
+		int count = 0;
+		do {
+			tmp = result;
+			result = (2*result + value/(result*result))/3;
+			count++;
+			
+		} while (java.lang.Math.abs(result - tmp) > 0.000000000001);
+		System.out.println("count: " + count);
+		return result;
+		
 	}
 
 }
